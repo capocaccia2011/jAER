994e303cc170c9e6f84ddc5b7c49d57928d64ef7
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2011-05-11 15:46:23 +0200

    moved visualisation to separate function so that we can paint estimates of several KalmaFilters

--- host/java/src/ch/unizh/ini/jaer/projects/labyrinthkalman/KalmanEventFilter.java
+++ host/java/src/ch/unizh/ini/jaer/projects/labyrinthkalman/KalmanEventFilter.java
@@ -11,6 +11,8 @@ import net.sf.jaer.event.*;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
+import javax.vecmath.Color3f;
+
 import net.sf.jaer.Description;
 import net.sf.jaer.eventprocessing.EventFilter2D;
 import net.sf.jaer.graphics.FrameAnnotater;
@@ -157,6 +159,65 @@ public class KalmanEventFilter extends EventFilter2D implements FrameAnnotater {
     public boolean isAnnotationEnabled() {
         return annotationEnabled;
     }
+    
+    protected static void paintCovarianceEllipse( double[] pos, double[][] cov, final double nsigmas, Color3f color, GL gl )
+    {
+        final int no_points_ellipse = 12;
+
+        final double a = cov[0][0];
+        final double b = cov[1][0];
+        final double c = cov[1][1];
+        final double d = Math.sqrt( a*a + 4*b*b - 2*a*c + c*c );
+        final double scale1 = Math.sqrt( 0.5 * ( a+c+d ) ) * nsigmas;
+        final double scale2 = Math.sqrt( 0.5 * ( a+c-d ) ) * nsigmas;
+        final double theta = 0.5 * Math.atan2( (2*b), (a-c) ) * 180.0 / Math.PI;
+
+        gl.glPushMatrix();
+
+        gl.glTranslated(pos[0], pos[1], 0);
+        gl.glRotated(theta, 0, 0, 1);
+        gl.glScaled(scale1, scale2, 1);
+
+        gl.glColor3f( color.x, color.y, color.z );
+        gl.glLineWidth(2);    	
+
+        gl.glBegin(GL.GL_LINE_LOOP);
+        final double angle = 2*Math.PI / (double)no_points_ellipse;
+        for (int i = 0;i<no_points_ellipse;i++)
+            gl.glVertex2d( Math.cos( i*angle ), Math.sin( i*angle ) );
+        gl.glEnd();
+
+        gl.glPopMatrix();
+    }
+    
+    protected static void paintKalmanFilterState( final LabyrinthBallKalmanFilter filter, GL gl, Color3f covColor, Color3f speedColor )
+    {
+        final double[] mu = filter.getMu();
+        final double[][] Sigma = filter.getSigma();
+
+        paintCovarianceEllipse( mu, Sigma, 3, covColor, gl );
+
+        double velx = mu[2] * 0.1;
+        double vely = mu[3] * 0.1;
+
+        gl.glColor3f( speedColor.x, speedColor.y, speedColor.z );
+        gl.glLineWidth(4);
+
+        gl.glPushMatrix();
+        gl.glTranslated(mu[0], mu[1], 0);
+        gl.glBegin(GL.GL_LINES);
+        gl.glVertex2d(0, 0);
+        gl.glVertex2d(velx, vely);
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glLineWidth(1);
+        gl.glColor3f(1,1,1);
+        gl.glBegin(GL.GL_LINES);
+        gl.glVertex2d(64, 64);
+        gl.glVertex2d(mu[0], mu[1]);
+        gl.glEnd();    	
+    }
 
     @Override
     public void annotate(GLAutoDrawable drawable) {
@@ -168,54 +229,9 @@ public class KalmanEventFilter extends EventFilter2D implements FrameAnnotater {
             return;
 
         GL gl=drawable.getGL();
-
-        gl.glColor3f(1,1,0);
-        gl.glLineWidth(2);
-
-        int no_points_ellipse = 12;
-        double nsigmas = 3;
-
-        double a = kf.getSigma()[0][0];
-        double b = kf.getSigma()[1][0];
-        double c = kf.getSigma()[1][1];
-        double d = Math.sqrt( a*a + 4*b*b - 2*a*c + c*c );
-        double scale1 = Math.sqrt( 0.5 * ( a+c+d ) ) * nsigmas;
-        double scale2 = Math.sqrt( 0.5 * ( a+c-d ) ) * nsigmas;
-        double theta = 0.5 * Math.atan2( (2*b), (a-c) ) * 180.0 / Math.PI;
-
-        gl.glPushMatrix();
-        gl.glTranslated(kf.getMu()[0], kf.getMu()[1], 0);
-        gl.glRotated(theta, 0, 0, 1);
-        gl.glScaled(scale1, scale2, 1);
-        gl.glBegin(GL.GL_LINE_LOOP);
-        for (int i = 0;i<no_points_ellipse;i++){
-            double cc = Math.cos(2*Math.PI*i/(double)no_points_ellipse);
-            double ss = Math.sin(2*Math.PI*i/(double)no_points_ellipse);
-            gl.glVertex2d(cc,ss);
-        }
-        gl.glEnd();
-        gl.glPopMatrix();
-
-        double velx = kf.getMu()[2];
-        double vely = kf.getMu()[3];
-
-        gl.glColor3f(1,0,0);
-        gl.glLineWidth(4);
-
-        gl.glPushMatrix();
-        gl.glTranslated(kf.getMu()[0], kf.getMu()[1], 0);
-        gl.glBegin(GL.GL_LINES);
-        gl.glVertex2d(0, 0);
-        gl.glVertex2d(velx, vely);
-        gl.glEnd();
-        gl.glPopMatrix();
-
-        gl.glLineWidth(1);
-        gl.glColor3f(1,1,1);
-        gl.glBegin(GL.GL_LINES);
-        gl.glVertex2d(64, 64);
-        gl.glVertex2d(kf.getMu()[0], kf.getMu()[1]);
-        gl.glEnd();
+        Color3f covColor = new Color3f(1,1,0);
+        Color3f speedColor = new Color3f(1,0,0);
+        paintKalmanFilterState( kf, gl, covColor, speedColor );
     }
 
     public Point2D.Float getBallPosition() {
